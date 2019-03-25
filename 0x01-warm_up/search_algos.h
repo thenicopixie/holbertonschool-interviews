9440512309fd7e3cec0ddb3bcd40eafba21911a7
thenicopixie <391@holbertonschool.com>
2019-03-25 19:11:48 +0000

    change libary, add comments

--- 0x01-warm_up/search_algos.h
+++ 0x01-warm_up/search_algos.h
@@ -1,7 +1,12 @@
 #ifndef _SEARCH_ALGOS_H_
 #define _SEARCH_ALGOS_H_
-#include <stdlib.h>
+#include <stdio.h>
 
+
+/**
+ * binary_search - search for value in given array
+ * print_array - print sub array from given array
+ */
 int binary_search(int *array, size_t size, int value);
 void print_array(size_t left, size_t right, int *array);
 
