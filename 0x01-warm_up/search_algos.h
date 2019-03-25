0da56448f387f62a1916b04f358e78af8b9657c5
thenicopixie <391@holbertonschool.com>
2019-03-25 18:54:06 +0000

    header file for binary search

---
+++ 0x01-warm_up/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+#include <stdlib.h>
+
+int binary_search(int *array, size_t size, int value);
+void print_array(size_t left, size_t right, int *array);
+
+#endif /* _SEARCH_ALGOS_H_ */
